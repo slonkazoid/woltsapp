1de400d730778f74033162b8aed8cb10922d7a88
slonkazoid <slonkazoid@slonk.ing>
2024-07-15 18:31:10 +0300

    move main logic to loop

--- main.go
+++ main.go
@@ -6,14 +6,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
-	"go.mau.fi/whatsmeow"
-	//"go.mau.fi/whatsmeow/types/events"
 	_ "github.com/mattn/go-sqlite3"
 	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types/events"
 	waLog "go.mau.fi/whatsmeow/util/log"
 )
 
@@ -38,6 +35,40 @@ func initI18n(lang string) {
 			return key
 		}
 	}
+}
+
+func loop(mainLog waLog.Logger, loginLog waLog.Logger, clientLog waLog.Logger, qrLog waLog.Logger, container *sqlstore.Container) {
+	client, err := Login(loginLog, clientLog, qrLog, container)
+	if err == LoginTimeout {
+		mainLog.Errorf("%s, retrying after 5 seconds...", err)
+		time.Sleep(5 * time.Second)
+		return
+	} else if err != nil {
+		panic(err)
+	}
+
+	mainLog.Infof("initialized")
+
+	cRestart := make(chan struct{})
+
+	client.AddEventHandler(func(evt interface{}) {
+		mainLog.Debugf("got event %v", evt)
+		switch evt.(type) {
+		case events.PermanentDisconnect:
+			mainLog.Warnf("permanent disconnect, retrying...")
+			cRestart <- struct{}{}
+		case events.LoggedOut:
+			mainLog.Warnf("logged out, retrying...")
+			cRestart <- struct{}{}
+		}
+	})
+
+	_ = <-cRestart
+
+	mainLog.Infof("restarting...")
+
+	client.RemoveEventHandlers()
+	client.Disconnect()
 }
 
 func main() {
@@ -72,28 +103,7 @@ func main() {
 
 	dbLog.Infof("database ready")
 
-	var client *whatsmeow.Client
-
-loginStart:
-	client, err = Login(loginLog, clientLog, qrLog, container)
-	if err == LoginTimeout {
-		mainLog.Errorf("%s, retrying after 5 seconds...", err)
-		time.Sleep(5 * time.Second)
-		goto loginStart
-	} else if err != nil {
-		panic(err)
+	for {
+		loop(mainLog, loginLog, clientLog, qrLog, container)
 	}
-
-	mainLog.Infof("initialized")
-
-	client.AddEventHandler(func(evt interface{}) {
-		fmt.Println("got event:", evt)
-	})
-
-	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	<-c
-
-	client.Disconnect()
 }
