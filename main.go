4c2c254572c7cd9554228b7b1935a5ea3e5d8b6a
slonkazoid <slonkazoid@slonk.ing>
2024-07-15 18:33:04 +0300

    move bot function to bot.go

--- main.go
+++ main.go
@@ -6,11 +6,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 	"go.mau.fi/whatsmeow/store/sqlstore"
-	"go.mau.fi/whatsmeow/types/events"
 	waLog "go.mau.fi/whatsmeow/util/log"
 )
 
@@ -35,40 +33,6 @@ func initI18n(lang string) {
 			return key
 		}
 	}
-}
-
-func bot(mainLog waLog.Logger, loginLog waLog.Logger, clientLog waLog.Logger, qrLog waLog.Logger, container *sqlstore.Container) {
-	client, err := Login(loginLog, clientLog, qrLog, container)
-	if err == LoginTimeout {
-		mainLog.Errorf("%s, retrying after 5 seconds...", err)
-		time.Sleep(5 * time.Second)
-		return
-	} else if err != nil {
-		panic(err)
-	}
-
-	mainLog.Infof("initialized")
-
-	cRestart := make(chan struct{})
-
-	client.AddEventHandler(func(evt interface{}) {
-		mainLog.Debugf("got event %v", evt)
-		switch evt.(type) {
-		case events.PermanentDisconnect:
-			mainLog.Warnf("permanent disconnect, retrying...")
-			cRestart <- struct{}{}
-		case events.LoggedOut:
-			mainLog.Warnf("logged out, retrying...")
-			cRestart <- struct{}{}
-		}
-	})
-
-	_ = <-cRestart
-
-	mainLog.Infof("restarting...")
-
-	client.RemoveEventHandlers()
-	client.Disconnect()
 }
 
 func main() {
@@ -104,6 +68,6 @@ func main() {
 	dbLog.Infof("database ready")
 
 	for {
-		bot(mainLog, loginLog, clientLog, qrLog, container)
+		Bot(mainLog, loginLog, clientLog, qrLog, container)
 	}
 }
